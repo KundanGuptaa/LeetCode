fd1984867e1798546b8f0e57afcb6f53ef9588fe
Kundan Gupta <kundankumarsobh@gmail.com>
2026-08-23 23:58:03 +0530

    Time: 3 ms (97.19%), Space: 163.1 MB (53.99%) - LeetHub

---
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+        int[] componentId = new int[n];
+        int currComponent = 0;
+        componentId[0] = 0;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] - nums[i - 1] <= maxDiff) {
+                componentId[i] = currComponent;
+            } else {
+                currComponent++;
+                componentId[i] = currComponent;
+            }
+        }
+        boolean[] answer = new boolean[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int u = queries[i][0];
+            int v = queries[i][1];
+            answer[i] = (componentId[u] == componentId[v]);
+        }
+        return answer;
+    }
+}
